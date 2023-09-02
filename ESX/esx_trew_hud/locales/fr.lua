c2ae788c965affd45b614e22691e89bb35751c84
Dominic Stanfford <64571534+Manta95@users.noreply.github.com>
2023-09-02 04:37:15 +0200

    Added French translation

---
+++ ESX/esx_trew_hud/locales/fr.lua
@@ -0,0 +1,71 @@
+Locales['en'] = {
+	['you_are_on_location'] = 'Tu es sur <strong>%s</strong>',
+
+	['date_format'] = {
+		default = 'On est le <strong>%s of %s, %s</strong>',
+		simple = 'On est le <strong>%s, %s</strong>',
+		simpleWithHours = 'Il est <strong>%s</strong>, <strong>%s, %s</strong>',
+		withWeekday = 'On est le <strong>%s, %s de %s, %s</strong>',
+		withHours = 'Il est <strong>%s</strong>, <strong>%s de %s, %s</strong>',
+		withWeekdayAndHours = 'Il est <strong>%s</strong>, <strong>%s, %s de %s, %s</strong>'
+	},
+
+	['weekDay_0'] = 'Dimanche',
+	['weekDay_1'] = 'Lundi',
+	['weekDay_2'] = 'Mardi',
+	['weekDay_3'] = 'Mercredi',
+	['weekDay_4'] = 'Jeudi',
+	['weekDay_5'] = 'Vendredi',
+	['weekDay_6'] = 'Samedi',
+
+	['day_1'] = '1st',
+	['day_2'] = '2',
+	['day_3'] = '3',
+	['day_4'] = '4',
+	['day_5'] = '5',
+	['day_6'] = '6',
+	['day_7'] = '7',
+	['day_8'] = '8',
+	['day_9'] = '9',
+
+	['day_10'] = '10',
+	['day_11'] = '11',
+	['day_12'] = '12',
+	['day_13'] = '13',
+	['day_14'] = '14',
+	['day_15'] = '15',
+	['day_16'] = '16',
+	['day_17'] = '17',
+	['day_18'] = '18',
+	['day_19'] = '19',
+
+	['day_20'] = '20',
+	['day_21'] = '21',
+	['day_22'] = '22',
+	['day_23'] = '23',
+	['day_24'] = '24',
+	['day_25'] = '25',
+	['day_26'] = '26',
+	['day_27'] = '27',
+	['day_28'] = '28',
+	['day_29'] = '29',
+
+	['day_30'] = '30',
+	['day_31'] = '31',
+
+	['month_0'] = 'Janvier',
+	['month_1'] = 'Février',
+	['month_2'] = 'Mars',
+	['month_3'] = 'Avril',
+	['month_4'] = 'Mai',
+	['month_5'] = 'Juin',
+	['month_6'] = 'July',
+	['month_7'] = 'Aout',
+	['month_8'] = 'Septembre',
+	['month_9'] = 'Octobre',
+	['month_10'] = 'Novembre',
+	['month_11'] = 'Decembre',
+
+	['toggleui'] = 'Active / désactive certaines parties du HUD',
+
+}
